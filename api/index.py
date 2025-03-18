fb0ea05eab9c745909678cfb37c5bf25c5c94184
ryanimal <ryanlvv@gmail.com>
2025-03-17 22:01:13 -0700

    Fix Vercel configuration to allow POST requests

--- api/index.py
+++ api/index.py
@@ -6,7 +6,8 @@ import os
 # Add the project root to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Import the app from the main API module
 from src.python_ai_bot.api import app
 
-# This app object is used by Vercel serverless functions
+# This is used by Vercel serverless functions
 app = app 
